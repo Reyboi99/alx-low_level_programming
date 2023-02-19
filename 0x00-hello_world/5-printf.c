bffde658031a1081b92a15192be18438a17e9c94
Ntim-Manteaw Reynolds <ntimmanteawr@gmail.com>
2023-02-19 15:02:36 -0800

    coding four

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - using a programme that prints using printf function
  * Return: 0 (Success)
  */
-int main(void)
+int main(void);
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art, \n")
+return (0);
 }
