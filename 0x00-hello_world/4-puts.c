a476bfd01b988fca558f5c72d6f18edc96e24d56
Ntim-Manteaw Reynolds <ntimmanteawr@gmail.com>
2023-02-19 07:38:07 -0800

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing puts functions
+ * Return: 0 (Done)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle
+");
+return (0);
+}
