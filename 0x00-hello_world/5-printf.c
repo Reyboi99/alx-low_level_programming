c4b873c8573d822138e50237d198f496a544b8a3
Ntim-Manteaw Reynolds <ntimmanteawr@gmail.com>
2023-02-20 14:45:00 -0800

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - using a programme that prints using printf function
- * Return: 0 (SUCCESS)
+ * Return: 0 (Success)
  */
-int main(void);
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
 }
