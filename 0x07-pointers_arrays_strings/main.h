26368fd827014f4162cc25173e2eff0e2973b9fd
Ntim-Manteaw Reynolds <ntimmanteawr@gmail.com>
2023-03-04 09:05:25 -0800

    back

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file - main.h
+ * auth - reynolds
+ * desc - prototypes of all functions in directory.
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_strng(char **s, char *to);
+
+#endif
