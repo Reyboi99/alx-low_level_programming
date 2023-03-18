613932ab08e6ff9f4d4b2181b1a6578fe5525c11
Ntim-Manteaw Reynolds <ntimmanteawr@gmail.com>
2023-03-18 06:12:52 -0700

    worse

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * cap_string - captalizes all words of a string
+ * @str: The string to be capitalized..
+ * Return: a pointer to the changed string
+ */
+char *cap_string(char *str)
+{
+	int index = 0:w
+
